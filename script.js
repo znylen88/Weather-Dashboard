358626eb583342b5bb9ed1f527b790de45848a91
Z <z@Zacharys-MacBook-Air.local>
2021-01-22 18:02:31 -0800

    Update styling and consolidate code

--- script.js
+++ script.js
@@ -4,23 +4,10 @@ var searchForm = document.getElementById("searchForm");
 var searchInput = document.getElementById("searchInput");
 
 searchForm.addEventListener("submit", function (event) {
-
-    if (searchInput.value === "") {
-        return null;
-
-    }
-
     localStorage.setItem("City", (searchInput.value));
-
 });
 
 // Current Date
-
-// function stoppedTyping() {
-//     if (searchInput.value == "") {
-//     document.getElementById('start_button').disabled = false;
-// } else {
-//     document.getElementById('start_button').disabled = true;
 
 var currentDate = moment().format("MMM Do YY");
 
@@ -32,7 +19,7 @@ var savedCity = localStorage.getItem("City");
 // Running AJAX current weather call to the OpenWeatherMap API
 
 $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + savedCity + ",usa&appid=" + APIKey,
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + savedCity + "&units=imperial&appid=" + APIKey,
     method: "GET"
 })
 
@@ -44,41 +31,19 @@ $.ajax({
 
         $("#cityName").text(response.name + " (" + currentDate + ")");
 
-        if ((response.weather[0].main) === "Clouds") {
-            $("#cityName").append(" " + '<i class="fas fa-cloud">');
-        }
-        if ((response.weather[0].main) === "Clear") {
-            $("#cityName").append(" " + '<i class="fas fa-sun">');
-        }
-        if ((response.weather[0].main) === "Rain") {
-            $("#cityName").append(" " + '<i class="fas fa-cloud-rain">');
-        }
-        if ((response.weather[0].main) === "Snow") {
-            $("#cityName").append(" " + '<i class="far fa-snowflake">');
-        }
-        if ((response.weather[0].main) === "Fog") {
-            $("#cityName").append(" " + '<i class="fas fa-smog">');
-        }
-        if ((response.weather[0].main) === "Mist") {
-            $("#cityName").append(" " + '<i class="fas fa-smog">');
-        }
-
         // Grab current temperature info and convert to Farenheit and round to one decimal place
-
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        var roundedTempF = Math.round(tempF * 10) / 10;
+        var roundedTempF = Math.round(response.main.temp);
+        var roundedWind = Math.round(response.wind.speed);
+        var iconcode = response.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
         // Append info to the current weather divs
 
         $("#temperature").text("Temperature: " + roundedTempF + " °F");
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
-        $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+        $("#windSpeed").text("Wind Speed: " + roundedWind + " MPH");
         $("#uvIndex").text("UV Index: ");
-
-        var newDiv = $("<div>")
-        $("#innerContainer").append(newDiv);
-        newDiv.text(savedCity);
-        newDiv.css({ "height": "auto", "line-height": "2.5", "font-size": "30px" });
+        $('#wicon').attr('src', iconurl);
 
         var cityLat = response.coord.lat;
         var cityLng = response.coord.lon;
@@ -105,7 +70,7 @@ $.ajax({
 // Running AJAX 5 day forecast call to the OpenWeatherMap API
 
 $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + savedCity + ",usa&appid=" + APIKey,
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + savedCity + "&units=imperial&appid=" + APIKey,
     method: "GET"
 })
 
@@ -118,34 +83,18 @@ $.ajax({
         for (var i = 0; i < response.list.length - 32; i++) {
 
             var datePlus1 = moment().add(1, 'days').format("MMM Do YY");
-            var forecastTempDay1F = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-            var roundedDay1 = Math.round(forecastTempDay1F * 10) / 10;
+            var forecastTempDay1 = response.list[i].main.temp;
+            var roundedDay1 = Math.round(forecastTempDay1);
+            var iconcode1 = response.list[i].weather[0].icon;
+            var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
             var timeSlice = response.list[i].dt_txt.slice(11);
 
             if (timeSlice === "18:00:00") {
 
                 $("#weatherBoxDate1").text(datePlus1);
-                $("#weatherBoxTemp1").text("Temp: " + roundedDay1 + " °F");
+                $('#wicon1').attr('src', iconurl1);
+                $("#weatherBoxTemp1").text(roundedDay1 + "°");
                 $("#weatherBoxHum1").text("Humidity: " + response.list[i].main.humidity + "%");
-
-                if ((response.list[i].weather[0].main) === "Clouds") {
-                    $("#weatherDescription1").append(" " + '<i class="fas fa-cloud">');
-                }
-                if ((response.list[i].weather[0].main) === "Clear") {
-                    $("#weatherDescription1").append(" " + '<i class="fas fa-sun">');
-                }
-                if ((response.list[i].weather[0].main) === "Rain") {
-                    $("#weatherDescription1").append(" " + '<i class="fas fa-cloud-rain">');
-                }
-                if ((response.list[i].weather[0].main) === "Snow") {
-                    $("#weatherDescription1").append(" " + '<i class="far fa-snowflake">');
-                }
-                if ((response.list[i].weather[0].main) === "Fog") {
-                    $("#weatherDescription1").append(" " + '<i class="fas fa-smog">');
-                }
-                if ((response.list[i].weather[0].main) === "Mist") {
-                    $("#weatherDescription1").append(" " + '<i class="fas fa-smog">');
-                }
             }
         }
 
@@ -154,34 +103,18 @@ $.ajax({
         for (var i = 8; i < response.list.length - 24; i++) {
 
             var datePlus2 = moment().add(2, 'days').format("MMM Do YY");
-            var forecastTempDay2F = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-            var roundedDay2 = Math.round(forecastTempDay2F * 10) / 10;
+            var forecastTempDay2 = (response.list[i].main.temp);
+            var roundedDay2 = Math.round(forecastTempDay2);
+            var iconcode2 = response.list[i].weather[0].icon;
+            var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
             var timeSlice = response.list[i].dt_txt.slice(11);
 
             if (timeSlice === "18:00:00") {
 
                 $("#weatherBoxDate2").text(datePlus2);
-                $("#weatherBoxTemp2").text("Temp: " + roundedDay2 + " °F");
+                $("#weatherBoxTemp2").text(roundedDay2 + "°");
                 $("#weatherBoxHum2").text("Humidity: " + response.list[i].main.humidity + "%");
-
-                if ((response.list[i].weather[0].main) === "Clouds") {
-                    $("#weatherDescription2").append(" " + '<i class="fas fa-cloud">');
-                }
-                if ((response.list[i].weather[0].main) === "Clear") {
-                    $("#weatherDescription2").append(" " + '<i class="fas fa-sun">');
-                }
-                if ((response.list[i].weather[0].main) === "Rain") {
-                    $("#weatherDescription2").append(" " + '<i class="fas fa-cloud-rain">');
-                }
-                if ((response.list[i].weather[0].main) === "Snow") {
-                    $("#weatherDescription2").append(" " + '<i class="far fa-snowflake">');
-                }
-                if ((response.list[i].weather[0].main) === "Fog") {
-                    $("#weatherDescription2").append(" " + '<i class="fas fa-smog">');
-                }
-                if ((response.list[i].weather[0].main) === "Mist") {
-                    $("#weatherDescription1").append(" " + '<i class="fas fa-smog">');
-                }
+                $('#wicon2').attr('src', iconurl2);
             }
         }
 
@@ -190,34 +123,18 @@ $.ajax({
         for (var i = 16; i < response.list.length - 16; i++) {
 
             var datePlus3 = moment().add(3, 'days').format("MMM Do YY");
-            var forecastTempDay3F = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-            var roundedDay3 = Math.round(forecastTempDay3F * 10) / 10;
+            var forecastTempDay3 = (response.list[i].main.temp);
+            var roundedDay3 = Math.round(forecastTempDay3);
+            var iconcode3 = response.list[i].weather[0].icon;
+            var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
             var timeSlice = response.list[i].dt_txt.slice(11);
 
             if (timeSlice === "18:00:00") {
 
                 $("#weatherBoxDate3").text(datePlus3);
-                $("#weatherBoxTemp3").text("Temp: " + roundedDay3 + " °F");
+                $("#weatherBoxTemp3").text(roundedDay3 + "°");
                 $("#weatherBoxHum3").text("Humidity: " + response.list[i].main.humidity + "%");
-
-                if ((response.list[i].weather[0].main) === "Clouds") {
-                    $("#weatherDescription3").append(" " + '<i class="fas fa-cloud">');
-                }
-                if ((response.list[i].weather[0].main) === "Clear") {
-                    $("#weatherDescription3").append(" " + '<i class="fas fa-sun">');
-                }
-                if ((response.list[i].weather[0].main) === "Rain") {
-                    $("#weatherDescription3").append(" " + '<i class="fas fa-cloud-rain">');
-                }
-                if ((response.list[i].weather[0].main) === "Snow") {
-                    $("#weatherDescription3").append(" " + '<i class="far fa-snowflake">');
-                }
-                if ((response.list[i].weather[0].main) === "Fog") {
-                    $("#weatherDescription3").append(" " + '<i class="fas fa-smog">');
-                }
-                if ((response.list[i].weather[0].main) === "Mist") {
-                    $("#weatherDescription3").append(" " + '<i class="fas fa-smog">');
-                }
+                $('#wicon3').attr('src', iconurl3);
             }
         }
 
@@ -226,34 +143,18 @@ $.ajax({
         for (var i = 24; i < response.list.length - 8; i++) {
 
             var datePlus4 = moment().add(4, 'days').format("MMM Do YY");
-            var forecastTempDay4F = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-            var roundedDay4 = Math.round(forecastTempDay4F * 10) / 10;
+            var forecastTempDay4 = (response.list[i].main.temp);
+            var roundedDay4 = Math.round(forecastTempDay4);
+            var iconcode4 = response.list[i].weather[0].icon;
+            var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
             var timeSlice = response.list[i].dt_txt.slice(11);
 
             if (timeSlice === "18:00:00") {
 
                 $("#weatherBoxDate4").text(datePlus4);
-                $("#weatherBoxTemp4").text("Temp: " + roundedDay4 + " °F");
+                $("#weatherBoxTemp4").text(roundedDay4 + "°");
                 $("#weatherBoxHum4").text("Humidity: " + response.list[i].main.humidity + "%");
-
-                if ((response.list[i].weather[0].main) === "Clouds") {
-                    $("#weatherDescription4").append(" " + '<i class="fas fa-cloud">');
-                }
-                if ((response.list[i].weather[0].main) === "Clear") {
-                    $("#weatherDescription4").append(" " + '<i class="fas fa-sun">');
-                }
-                if ((response.list[i].weather[0].main) === "Rain") {
-                    $("#weatherDescription4").append(" " + '<i class="fas fa-cloud-rain">');
-                }
-                if ((response.list[i].weather[0].main) === "Snow") {
-                    $("#weatherDescription4").append(" " + '<i class="far fa-snowflake">');
-                }
-                if ((response.list[i].weather[0].main) === "Fog") {
-                    $("#weatherDescription4").append(" " + '<i class="fas fa-smog">');
-                }
-                if ((response.list[i].weather[0].main) === "Mist") {
-                    $("#weatherDescription4").append(" " + '<i class="fas fa-smog">');
-                }
+                $('#wicon4').attr('src', iconurl4);
             }
         }
 
@@ -262,35 +163,18 @@ $.ajax({
         for (var i = 32; i < response.list.length; i++) {
 
             var datePlus5 = moment().add(5, 'days').format("MMM Do YY");
-            var forecastTempDay5F = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-            var roundedDay5 = Math.round(forecastTempDay5F * 10) / 10;
+            var forecastTempDay5 = (response.list[i].main.temp);
+            var roundedDay5 = Math.round(forecastTempDay5);
+            var iconcode5 = response.list[i].weather[0].icon;
+            var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
             var timeSlice = response.list[i].dt_txt.slice(11);
 
             if (timeSlice === "18:00:00") {
 
                 $("#weatherBoxDate5").text(datePlus5);
-                $("#weatherBoxTemp5").text("Temp: " + roundedDay5 + " °F");
+                $("#weatherBoxTemp5").text(roundedDay5 + "°");
                 $("#weatherBoxHum5").text("Humidity: " + response.list[i].main.humidity + "%");
-
-                if ((response.list[i].weather[0].main) === "Clouds") {
-                    $("#weatherDescription5").append(" " + '<i class="fas fa-cloud">');
-                }
-                if ((response.list[i].weather[0].main) === "Clear") {
-                    $("#weatherDescription5").append(" " + '<i class="fas fa-sun">');
-                }
-                if ((response.list[i].weather[0].main) === "Rain") {
-                    $("#weatherDescription5").append(" " + '<i class="fas fa-cloud-rain">');
-                }
-                if ((response.list[i].weather[0].main) === "Snow") {
-                    $("#weatherDescription5").append(" " + '<i class="far fa-snowflake">');
-                }
-                if ((response.list[i].weather[0].main) === "Fog") {
-                    $("#weatherDescription5").append(" " + '<i class="fas fa-smog">');
-                }
-                if ((response.list[i].weather[0].main) === "Mist") {
-                    $("#weatherDescription5").append(" " + '<i class="fas fa-smog">');
-                }
-
+                $('#wicon5').attr('src', iconurl5);
             }
         }
     });
